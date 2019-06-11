b720ac5621623eb825d1d1d8898560a15689dc48
Prateep Bandharangshi <prateep@prateep.io>
2019-06-11 12:50:49 +0100

    Update terraform outputs

--- ec2/outputs.tf
+++ ec2/outputs.tf
@@ -1,23 +1,11 @@
-output "T-Pot Admin UI" {
+output "Admin_UI" {
   value = "https://${aws_instance.tpot.public_dns}:64294/"
 }
 
-output "T-Pot SSH Access" {
-  value = "ssh -i {private_key_file} -p 64295 ubuntu@${aws_instance.tpot.public_dns}"
+output "SSH_Access" {
+  value = "ssh -i {private_key_file} -p 64295 admin@${aws_instance.tpot.public_dns}"
 }
 
-output "T-Pot Web UI" {
+output "Web_UI" {
   value = "https://${aws_instance.tpot.public_dns}:64297/"
-}
-
-output "EC2 Security group has been configured to allow admin access from" {
-  value = "${var.admin_ip}"
-}
-
-output "EC2 VPC ID" {
-  value = "${var.ec2_vpc_id}"
-}
-
-output "EC2 subnet ID" {
-  value = "${var.ec2_subnet_id}"
 }
